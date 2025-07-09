8519e77bb57cd733a4a467a2a2469cc74c004d0b
USER <USER@192.168.1.53>
2025-07-09 21:02:38 +0530

    Third commit

---
+++ samplemaven1/src/main/java/encapsulation/Encapsulation2.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Encapsulation2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation1 es =new Encapsulation1();
+		es.setter("Annie", 12);
+		es.getter();
+				
+	}
+
+}
